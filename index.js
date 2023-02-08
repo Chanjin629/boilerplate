244115d1ee1932a177b9905f934d8a1548f9f501
Kim, Chanjin <ihercwdxz@gmail.com>
2023-02-08 22:49:40 +0900

    node.js 14강까지 수강 - postman에러 발생

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
-
+const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 //application/json
 app.use(bodyParser.json());
 app.use(cookieParser());
+
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', true);
 mongoose.connect(config.mongoURI)
@@ -21,7 +22,7 @@ mongoose.connect(config.mongoURI)
 
 app.get('/', (req, res) => {res.send('Hello World!')})
 //아직 client 부분을 하지않아서 postman을 통해서 가져온다/
-app.post('/register', (req, res) => {
+app.post('api/users/register', (req, res) => {
   //회원 가입 할때 필요한 정보들을 client에서 가져오면
   //그것들을 데이터베이스에 넣어준다.
 
@@ -36,7 +37,7 @@ app.post('/register', (req, res) => {
     })
 }) 
 
-app.post('/login', (req, res)=>{
+app.post('api/users/login', (req, res)=>{
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({email: req.body.email },(err, user)=>{
     if(!user) {
@@ -44,7 +45,7 @@ app.post('/login', (req, res)=>{
         loginSuccess: false,
         message: "제공된 이메일에 해당하는 유저가 없습니다."
       })
-    }
+    } 
 
   //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
     user.comparePassword(req.body.password , (err,isMatch)=>{
@@ -65,5 +66,32 @@ app.post('/login', (req, res)=>{
   
   
 })
-  
+app.get('api/users/auth', auth , (req, res)=>{
+  //여가 까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True 라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    //role 1어드먼, role 2 특정부서 어드먼,
+    // role 0 ->일반유저, role 0이니면 관리자
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+  // console.log('req.user', req.user)
+  User.findOneAndUpdate({ _id: req.user._id },
+    { token: "" }
+    , (err, user) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      })
+    })
+})
 app.listen(port, () => {console.log(`Example app listening on port ${port}`)})
