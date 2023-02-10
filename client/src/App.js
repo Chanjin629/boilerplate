86a035c37588b722efed3a9f72b494dab16ea113
Kim, Chanjin <ihercwdxz@gmail.com>
2023-02-10 22:35:42 +0900

    주석 참고 요망

--- client/src/App.js
+++ client/src/App.js
@@ -14,11 +14,11 @@ function App() {
     <Router>
       <div>
         {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
+          강의를 보면 <Routes> 부분에 <Switch>를 쓰고
+          <element>를 <component>로 쓰고
+          {<LandingPage/>} 를 {LandingPage}로 썼지만 
+          버전 업데이트로 인해 조금 달라서 강의 그대로 하면 
+          에러가뜬다.
         */}
         <Routes>
           <Route exact path="/" element={<LandingPage/>}/>
